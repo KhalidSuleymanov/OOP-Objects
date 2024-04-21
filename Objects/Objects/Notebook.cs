ded9d42ec153f6c25f03c9516cf8aa683db5cea2
Khalid Suleymanov <167674232+KhalidSuleymanov@users.noreply.github.com>
2024-04-21 16:23:21 +0400

    Add files via upload

---
+++ Objects/Objects/Notebook.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HomeTask260123
+{
+    internal class Notebook
+    {
+        public string Brand;
+        public string Model;
+        public double Price;
+
+        public Notebook(string Brand, string Model, double Price = 0)
+        {
+            this.Brand = Brand;
+            this.Model = Model;
+            this.Price = Price;
+        }
+
+        public string ShowInfo()
+        {
+            return $"Brand: {Brand}, Model: {Model}, Price: {Price}";
+        }
+    }
+}
---
+++ Objects/Objects/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace HomeTask260123
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           Notebook[] notes = new Notebook[5];
+
+            for (int i = 0; i < notes.Length; i++)
+            {
+                Console.WriteLine("Brand of product: ");
+                string Brand = Console.ReadLine();
+
+                Console.WriteLine("Model of product: ");
+                string Model = Console.ReadLine();
+
+                double price;
+                do
+                {
+                    Console.WriteLine("Price of product: ");
+                    string Price = Console.ReadLine();
+                    price = Convert.ToDouble(Price);
+
+                } while (price <= 0);
+
+                Notebook Pc = new Notebook(Brand, Model, price);
+                notes[i] = Pc;
+            }
+
+            double sum = 0;
+            for (int i = 0; i < notes.Length; i++)
+            {
+                sum += notes[i].Price;
+
+            }
+            double avarage = sum / notes.Length;
+
+            Console.WriteLine(avarage);
+        }
+    }
+}
